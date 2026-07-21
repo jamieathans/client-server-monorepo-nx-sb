4ea1f579936b02fb3f5618fb8c09d5a88c18ba0b
Jamie Athanassakis <jamie_athans@hotmail.com>
2026-07-21 12:09:38 +0930

    Initial Mantine setup

--- client/apps/web/src/app/app.tsx
+++ client/apps/web/src/app/app.tsx
@@ -1,15 +1,21 @@
+import { createTheme, MantineProvider } from '@mantine/core';
+
 // Uncomment this line to use CSS modules
 // import styles from './app.module.css';
-import { OrgSharedUi } from '@org/shared-ui';
-import { sharedUtils } from '@org/shared-utils';
+//import { OrgSharedUi } from '@org/shared-ui';
+//import { sharedUtils } from '@org/shared-utils';
 //import NxWelcome from './nx-welcome';
+
+const theme = createTheme({/** Put your mantine theme override here */});
+
+function Root() {
+  return <div>App</div>;
+}
 
 export function App() {
   return (
-    <div>
-      {/* <NxWelcome title="@org/web" /> */}
-      <OrgSharedUi />
-      {sharedUtils()}
-    </div>
+    <MantineProvider theme={theme}>
+      <Root />
+    </MantineProvider>
   );
 }
