c1b27a9328f06e8816f3111c76ea66a93b41f765
Jamie Athanassakis <jamie_athans@hotmail.com>
2026-07-23 16:38:55 +0930

    Added children prop to NavLink

--- client/packages/shared-ui/src/lib/components/nav-link/nav-link.tsx
+++ client/packages/shared-ui/src/lib/components/nav-link/nav-link.tsx
@@ -2,11 +2,21 @@ import { NavLink as RouterNavLink, To } from 'react-router';
 import { NavLink as MantineNavLink } from '@mantine/core';
 import { ReactNode } from 'react';
 
-export function NavLink({ label, to }: { label: ReactNode; to: To }) {
+export function NavLink({
+  label,
+  to,
+  children,
+}: {
+  label: ReactNode;
+  to: To;
+  children?: ReactNode;
+}) {
   return (
     <MantineNavLink
       label={label}
       renderRoot={(props) => <RouterNavLink to={to} end {...props} />}
-    />
+    >
+      {children}
+    </MantineNavLink>
   );
 }
