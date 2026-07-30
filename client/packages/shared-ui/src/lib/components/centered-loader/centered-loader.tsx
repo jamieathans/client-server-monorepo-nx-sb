2a33039a162d691ee7d32b810204f11ea253e9d8
Jamie Athanassakis <jamie_athans@hotmail.com>
2026-07-30 14:11:52 +0930

    CenteredLoader props

--- client/packages/shared-ui/src/lib/components/centered-loader/centered-loader.tsx
+++ client/packages/shared-ui/src/lib/components/centered-loader/centered-loader.tsx
@@ -1,10 +1,29 @@
 import { Center, Loader } from '@mantine/core';
 import classes from './centered-loader.module.css';
+import { clsx } from 'clsx';
 
-export function CenteredLoader() {
+export function CenteredLoader({
+  fullScreen,
+  className,
+  classNames,
+}: {
+  fullScreen?: boolean;
+  className?: boolean;
+  classNames?: {
+    root?: string;
+    loader?: string;
+  };
+}) {
   return (
-    <Center className={classes.root}>
-      <Loader color="blue" size="xl" />
+    <Center
+      className={clsx(
+        classes.root,
+        className,
+        classNames?.root,
+        fullScreen && classes.fullScreen,
+      )}
+    >
+      <Loader color="blue" size="xl" className={clsx(classNames?.loader)} />
     </Center>
   );
 }
