e1dd22340cbca1190f38673da47e25efc6d0898d
Jamie Athanassakis <jamie_athans@hotmail.com>
2026-08-04 14:00:21 +0930

    Formatting and comments

--- build.sh
+++ build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# maven process-classess will first generate dto.d.ts which is required by the front end build.
 pushd server
 ./mvnw clean
 ./mvnw process-classes
 popd
 
+# build front end static assets (needs dto.d.ts).
 pushd client
 if [ "$1" == "ci" ]; then
-  npm ci
+	npm ci
 fi
 npm run build
 popd
 
+# build the jar which also packages the static front end assets.
 pushd server
 ./mvnw install -DskipTests
 popd
