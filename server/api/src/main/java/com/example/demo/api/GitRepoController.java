6b7bcf5eccea88cfb5a4eb11b28d053b2f550f38
Jamie Athanassakis <jamie_athans@hotmail.com>
2026-08-06 18:45:09 +0930

    Added GitRepoService

--- server/api/src/main/java/com/example/demo/api/GitRepoController.java
+++ server/api/src/main/java/com/example/demo/api/GitRepoController.java
@@ -4,23 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.GitRepoPropertiesDto;
-import com.example.demo.properties.GitRepoProperties;
+import com.example.demo.service.GitRepoService;
 
 @RestController
 public class GitRepoController extends BaseRestController {
 
     private static final String API_PREFIX_PATH = "/git-repo";
 
-    private final GitRepoProperties gitRepoProperties;
+    private final GitRepoService gitRepoService;
 
-    public GitRepoController(GitRepoProperties gitRepoProperties) {
-        this.gitRepoProperties = gitRepoProperties;
+    public GitRepoController(GitRepoService gitRepoService) {
+        this.gitRepoService = gitRepoService;
     }
 
     public static final String PROPERTIES_PATH = API_PREFIX_PATH + "/properties";
 
     @GetMapping(PROPERTIES_PATH)
     public GitRepoPropertiesDto getProperties() {
-        return new GitRepoPropertiesDto(gitRepoProperties.getCommitId());
+        return gitRepoService.getProperties();
     }
 }
