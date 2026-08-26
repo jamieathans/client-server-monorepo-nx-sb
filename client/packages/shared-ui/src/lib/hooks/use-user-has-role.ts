fc7548ebc2ef01eb6f476a61208dbc6510d73d96
Jamie Athanassakis <jamie_athans@hotmail.com>
2026-08-26 16:40:42 +0930

    Example of useQuery "select"

--- client/packages/shared-ui/src/lib/hooks/use-user-has-role.ts
+++ client/packages/shared-ui/src/lib/hooks/use-user-has-role.ts
@@ -3,14 +3,17 @@ import { useQuery } from '@tanstack/react-query';
 import { MeQueryFactory } from '../query-factories/me-query-factory';
 
 export function useUserHasRole() {
-  const meQuery = useQuery(MeQueryFactory.meQueryOptions());
+  const meQuery = useQuery({
+    ...MeQueryFactory.meQueryOptions(),
+    select: (data) => data.roles,
+  });
 
   function userHasRole(role: Role) {
     if (meQuery.data === undefined) {
       return undefined;
     }
 
-    const hasRole = meQuery.data.roles.find((value) => value === role);
+    const hasRole = meQuery.data.find((value) => value === role);
 
     return !!hasRole;
   }
